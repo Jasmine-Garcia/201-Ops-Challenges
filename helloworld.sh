29bb85bcff18521e7ac139c5d960733f32401a61
jazzy <jas.garcia305@gmail.com>
2023-02-18 04:42:05 +0000

    commit helloworld.sh file

---
+++ helloworld.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script:                   Ops 201 Class 02 Ops Challenge Solution
+# Author:                   Jasmine Garcia
+# Date of latest revision:  2/16/2023
+# Purpose:                  Print a string to the terminal
+
+# Main
+
+echo "I'm working perfectly"
+
+# End
