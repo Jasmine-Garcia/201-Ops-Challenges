47862632f403c7037c7c91b82f5568f90b2a9a08
jazzy <jas.garcia305@gmail.com>
2023-02-21 00:07:41 +0000

    resubmit assignment

---
+++ helloworldresubmit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script:                                  Resubmit: Ops 201 Class 02 Ops Challenge Solution
+# Author:                                  Jasmine Garcia
+# Date of latest revision:                 February 20, 2023
+# Purpose:                                 Redo: Print a string to the terminal
+
+
+# Main
+
+echo "I am working perfectly."
+
+# End
